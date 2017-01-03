420a002d7735641d9a30f20220892cfd28db4124
Dennis Meier <denwid@gmx.ch>
2017-01-03 16:07:57 +0000

    Initial commit

---
+++ test/predictionmarket.js
@@ -0,0 +1,7 @@
+contract('PredictionMarket', function(accounts) {
+  it("should assert true", function(done) {
+    var m = PredictionMarket.deployed();
+    assert.isTrue(true);
+    done();
+  });
+});
