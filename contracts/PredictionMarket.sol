531b898c6462b735558fee5f57ad46892a009c0f
Dennis Meier <denwid@gmx.ch>
2017-01-09 16:55:29 +0000

    Do basic implementation, not yet working

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -1,4 +1,85 @@
 pragma solidity ^0.4.2;
 
-contract PredictionMarket {
+contract owned {
+    function owned() { owner = msg.sender; }
+    address owner;
+}
+
+contract mortal is owned {
+    function kill() {
+        if (msg.sender == owner) selfdestruct(owner);
+    }
+}
+
+contract PredictionMarket is mortal {
+    Question[] public questions;
+
+    struct Bet {
+        address bettor;
+        uint stake;
+        bool predictedOutcome;
+    }
+
+    struct Question {
+        uint questionId;
+        string text;
+        bool resolved;
+        bool truth;
+        Bet[] bets;
+    }
+
+    function admin() returns (address) {
+        return owner;
+    }
+
+    function addQuestion(string questionText) returns (uint id) {
+        if (msg.sender != admin()) { throw; }
+        id = questions.length++;
+        //questions[id].questionId = id;
+        //questions[id].text = questionText;
+        //questions[id].resolved = false;
+        //questions[id].truth = false;
+        return id;
+    }
+
+    function getQuestion(uint questionId) returns (string text) {
+        return questions[questionId].text;
+    }
+
+    function betQuestion(uint questionId, bool opinion) payable returns (uint betId) {
+        if (questions[questionId].resolved == true) { throw; }
+        betId = questions[questionId].bets.length;
+        questions[questionId].bets.push(Bet(msg.sender, msg.value, opinion));
+        return betId;
+    }
+
+    function resolveQuestion(uint questionId, bool truth) {
+        if (msg.sender != admin()) { throw; }
+        questions[questionId].truth = truth;
+        questions[questionId].resolved = true;
+        payoutQuestion(questionId);
+    }
+
+    function payoutQuestion(uint questionId) internal returns (uint leftover) {
+        if (questions[questionId].resolved == false) { throw; }
+        uint i;
+        uint totalValue;
+        uint totalValueWrongBets;
+        uint totalValueCorrectBets;
+        for (i = 0; i < questions[questionId].bets.length; i++) {
+            totalValue += questions[questionId].bets[i].stake;
+            if (questions[questionId].bets[i].predictedOutcome != questions[questionId].truth) {
+                totalValueWrongBets += questions[questionId].bets[i].stake;
+            } else {
+                totalValueCorrectBets += questions[questionId].bets[i].stake;
+            }
+        }
+
+        for (i = 0; i < questions[questionId].bets.length; i++) {
+            if (questions[questionId].bets[i].predictedOutcome == questions[questionId].truth) {
+                uint payout = questions[questionId].bets[i].stake*totalValue/totalValueCorrectBets;
+                if (questions[questionId].bets[i].bettor.send(payout) == false) { throw; }
+            }
+        }
+    }
 }
--- test/predictionmarket.js
+++ test/predictionmarket.js
@@ -4,4 +4,36 @@ contract('PredictionMarket', function(accounts) {
     assert.isTrue(true);
     done();
   });
+
+  it("sets admin account correctly", function() {
+    var m = PredictionMarket.deployed();
+    return m.admin.call().then(function(outAdminAddress) {
+      return assert.equal(outAdminAddress , accounts[0]);
+    })
+  });
+
+  it("allows to add 2 new questions", function() {
+    var m = PredictionMarket.deployed();
+    Q1Added = m.addQuestion.call("Will Trump be a good president?").then(function(questionId) {
+      return assert.strictEqual(questionId.valueOf(), 0);
+    });
+    Q2Added = m.addQuestion.call("Will you get rich?").then(function(questionId) {
+      return assert.strictEqual(questionId.valueOf(), 1);
+    });
+    return Q1Added && Q2Added;
+  });
+
+  it("can read the two questions", function() {
+    var m = PredictionMarket.deployed();
+    return m.getQuestion(0).then(function(questionText) {
+      return assert.equal(questionText, "Will Trump be a good president?");
+    });
+  });
+
+  it("can accept bets on a question", function() {
+    var m = PredictionMarket.deployed();
+    return m.betQuestion(0, 0).then(function(betId) {
+      return assert.equal(betId, 0);
+    })
+  });
 });
